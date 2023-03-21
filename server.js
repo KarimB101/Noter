3b6b397882339fa7c22ccf870ba13fe0fa9f2fed
KarimB101 <karim.baptiste@yahoo.com>
2023-03-21 00:42:24 -0500

    made notes array file and connect to server.js

--- server.js
+++ server.js
@@ -1,17 +1,38 @@
 const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
 // Initialize the app and create a port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up body parsing, static, and route middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+// Set up middleware to parse incoming request bodies
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Define a route to handle GET requests for retrieving notes
+app.get('/api/notes', (req, res) => {
+    // Read the notes from the JSON file
+    const notes = JSON.parse(fs.readFileSync('notes.json', 'utf8'));
+    res.json(notes);
+  });
+
+// Define a route to handle POST requests for saving new notes
+app.post('/api/notes', (req, res) => {
+    // Read the existing notes from the JSON file
+    const notes = JSON.parse(fs.readFileSync('notes.json', 'utf8'));
+    // Generate a new ID for the note
+    const id = Date.now().toString();
+    // Add the new note to the array of notes
+    const newNote = { id, ...req.body };
+    notes.push(newNote);
+    // Write the updated notes back to the JSON file
+    fs.writeFileSync('notes.json', JSON.stringify(notes));
+    // Respond with the new note
+    res.json(newNote);
+  });
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
